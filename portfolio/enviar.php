6b7a1a48f6b7c5e47262b6626aba50695a2afd4d
majoco <francicisco@gmail.com>
2023-06-08 17:08:56 -0400

    version 126

--- portfolio/enviar.php
+++ portfolio/enviar.php
@@ -3,7 +3,7 @@
 	$package = $_POST['package'];
 	$message = $_POST['message'];
 	$asunto = 'Formulario Rellenado';
-	$mensaje = "Nombre: ".$nombre."<br> Email: $email<br> Mensaje:".$message;
+	$mensaje = "package: ".$package."<br> Email: $email<br> Mensaje:".$message;
 
 
 	if(mail('francicisco@gmail.com', $asunto, $mensaje)){
